ff1ba82fffd1671fc419733f5ff42c2dbfa09a71
ushanetha <ushanetha187@gmail.com>
2026-09-17 23:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-from image_processor import ImageProcessor
 from caption_generator import CaptionGenerator
 from storage_manager import StorageManager
 
